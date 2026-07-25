d56dd4d5ce7cfeff57bfa74aba8a030a1629b504
ellietownsend <epaigetownsend@gmail.com>
2026-07-25 15:22:21 -0700

    create component to edit volunteers with additional css

--- src/services/volunteerList.js
+++ src/services/volunteerList.js
@@ -1,8 +1,6 @@
 import supabase from "../../supabase-client";
 
 export async function addVolunteer(volunteer){
-
-
     const {
         email,
         firstName,
@@ -13,28 +11,24 @@ export async function addVolunteer(volunteer){
         role,
     } = volunteer;
 
-    try{
-        const {data, error} = await supabase
-            .from('volunteer')
-            .insert(
-                {
-                    email,
-                    first_name: firstName,
-                    last_name: lastName,
-                    preferred_name: preferredName,
-                    birthdate,
-                    subject,
-                    role,
-                }
-            )
-        if(error){
-            return {success: false, error: error.message};
-        }
-        return {success: true, error: null};
-    }catch(error){
-         return {success: false, error: error.message};
-
+ 
+    const {data, error} = await supabase
+        .from('volunteer')
+        .insert(
+            {
+                email,
+                first_name: firstName,
+                last_name: lastName,
+                preferred_name: preferredName,
+                birthdate,
+                subject,
+                role,
+            }
+        )
+    if(error){
+        return {success: false, error: error.message};
     }
+    return {success: true, error: null};
 }
 
 
