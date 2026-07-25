f334ec19a8814b2486ea3d5bd3797c7e55850d0b
ellietownsend <epaigetownsend@gmail.com>
2026-07-25 15:17:45 -0700

    remove preferredName value

--- src/components/VolunteerList/AddVolunteer.jsx
+++ src/components/VolunteerList/AddVolunteer.jsx
@@ -1,36 +1,33 @@
-import { useState, useActionState, useEffect } from "react";
+import { useState, useActionState } from "react";
+import { classes, roles, isEqual, personalInformation, normalizePersonalInformation } from "../../utils/lib";
+import ShowSuccess from "./ShowSuccess";
+import "../../styles/VolunteerTable.css";
+
 
 export function AddVolunteer({onClose}){
+
+  /* Form submission logic */
   const [error, submitAction, isPending] = useActionState(
       async (prevSubmission, newVolunteer) => {
         const volunteer = {
           email: newVolunteer.get('email'),
           firstName: newVolunteer.get('firstName'), 
           lastName: newVolunteer.get('lastName'),
-          preferredName: newVolunteer.get('preferredName'),
-          lastVolunteerDate: newVolunteer.get('lastVolunteerDate'),
           birthdate: newVolunteer.get('birthdate'),
           role: newVolunteer.getAll('role'),
           subject: newVolunteer.getAll('subject'),
-          hoursContributed: newVolunteer.get('hoursContributed'),
         };
-
-        try{
-          const {success, error} = await addVolunteer(volunteer);
-          if(error){
-            console.log(error)
+        /* send volunteer information to supabase */
+        const {success, error} = await addVolunteer(volunteer); 
+          if(!success){
+            return {error: error}
           }
-          console.log("entered");
-        }catch(error){
-          console.log(error)
+        return null;
         }
-      }
-      
     ,[])
 
 
     return(
-    
      <div className="modal-overlay">
         <div className="modal">
           <div className="modal-header">
@@ -49,93 +46,24 @@ export function AddVolunteer({onClose}){
                 X
             </button>
 
-  </div>
+    </div>
 
     <form action={submitAction}>
 
       <h2 className="section-title">Personal Information</h2>
 
       <div className="form-grid">
-
-        <div className="field">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            required
-            name="email"
-            id="email"
-          />
-        </div>
-
-        <div className="field">
-          <label htmlFor="birthdate">Birthdate</label>
-          <input
-            type="date"
-            required
-            name="birthdate"
-            id="birthdate"
-          />
-        </div>
-
-        <div className="field">
-          <label htmlFor="firstName">First Name</label>
-          <input
-            type="text"
-            required
-            name="firstName"
-            id="firstName"
-          />
-        </div>
-
-        <div className="field">
-          <label htmlFor="preferredName">
-            Preferred Name
-            <span>Optional</span>
-          </label>
-
-          <input
-            type="text"
-            name="preferredName"
-            id="preferredName"
-          />
-        </div>
-
-        <div className="field">
-          <label htmlFor="lastName">Last Name</label>
-          <input
-            type="text"
-            required
-            name="lastName"
-            id="lastName"
-          />
-        </div>
-
-        <div className="field">
-          <label htmlFor="lastVolunteerDate">
-            Last Volunteer Date
-            <span>Optional</span>
-          </label>
-
-          <input
-            type="date"
-            name="lastVolunteerDate"
-            id="lastVolunteerDate"
-          />
-        </div>
-
-        <div className="field">
-          <label htmlFor="hoursContributed">
-            Hours Contributed
-            <span>Defaults to 0</span>
-          </label>
-
-          <input
-            type="number"
-            name="hoursContributed"
-            id="hoursContributed"
-          />
-        </div>
-
+         {personalInformation.map((item) => (
+            <div className="field" key = {item}>
+              <label> {normalizePersonalInformation(item)} </label>
+                  <input
+                      type={item}
+                      required
+                      name={item}
+                      id={item}
+                  />
+          </div>
+          ))}
       </div>
 
       <h2 className="section-title subjects">
@@ -143,123 +71,48 @@ export function AddVolunteer({onClose}){
       </h2>
 
       <div className="subject-group">
-
-      <label className="choice-card">
-    <input type="checkbox" name="subject" value="Math" />
-    <span>Math</span>
-</label>
-        <label className="choice-card">
-  <input type="checkbox" name="subject" value="Computer Science" />
-  <span>Computer Science</span>
-</label>
-
-<label className="choice-card">
-  <input type="checkbox" name="subject" value="AMC 8" />
-  <span>AMC 8</span>
-</label>
-
-<label className="choice-card">
-  <input type="checkbox" name="subject" value="AMC 10" />
-  <span>AMC 10</span>
-</label>
-
-<label className="choice-card">
-  <input type="checkbox" name="subject" value="Java" />
-  <span>Java</span>
-</label>
-
-<label className="choice-card">
-  <input type="checkbox" name="subject" value="Python" />
-  <span>Python</span>
-</label>
-
-<label className="choice-card">
-  <input type="checkbox" name="subject" value="Algebra 1" />
-  <span>Algebra 1</span>
-</label>
-
-<label className="choice-card">
-  <input type="checkbox" name="subject" value="Geometry" />
-  <span>Geometry</span>
-</label>
-
-<label className="choice-card">
-  <input
-    type="checkbox"
-    name="subject"
-    value="Algebra 2/ Pre-Calculus"
-  />
-  <span>Algebra 2 / Pre-Calculus</span>
-</label>
-
-<label className="choice-card">
-  <input type="checkbox" name="subject" value="Chemistry" />
-  <span>Chemistry</span>
-</label>
-
-<label className="choice-card">
-  <input
-    type="checkbox"
-    name="subject"
-    value="Introduction to Biology"
-  />
-  <span>Introduction to Biology</span>
-</label>
-
-<label className="choice-card">
-  <input type="checkbox" name="subject" value="Physics" />
-  <span>Physics</span>
-</label>
+         {classes.map((className) => (
+            <label key={className} className="choice-card">
+                <input
+                type="checkbox"
+                name="subject"
+                value = {className}
+                />
+                <span>{className}</span>
+            </label>
+            ))}
       </div>
 
-
       <h2 className="section-title roles">
-        Volunteer Roles
+        Roles:
       </h2>
 
       <div className="role-group">
-
-      <label className="choice-card">
-  <input type="checkbox" name="role" value="tutor" />
-  <span>Tutor</span>
-</label>
-
-<label className="choice-card">
-  <input type="checkbox" name="role" value="curriculum" />
-  <span>Curriculum</span>
-</label>
-
-<label className="choice-card">
-  <input type="checkbox" name="role" value="outreach" />
-  <span>Outreach</span>
-</label>
-
-<label className="choice-card">
-  <input type="checkbox" name="role" value="matching" />
-  <span>Student Matching</span>
-</label>
-
-<label className="choice-card">
-  <input type="checkbox" name="role" value="operations" />
-  <span>Program Operations</span>
-</label>
-      </div>
+        {roles.map((role) => (
+            <label key={role} className="choice-card">
+                <input 
+                type="checkbox" 
+                name="role" 
+                value = {role}
+                />
+            <span>{role}</span>
+            </label>
+            ))}
+        </div>
 
       <div className="modal-actions">
-    <button
-        type="submit"
-        className="submit-btn"
-    >
-        Add Volunteer
-    </button>
-</div>
+      <button
+          type="submit"
+          className="submit-btn"
+      >
+          Add Volunteer
+      </button>
+   </div>
 
-    </form>
+  {error || <ShowSuccess whatSucceeded = "volunteer_added"/> }
+  </form>
 
   </div>
 </div>
-    
-  
 );
-
 }
