1a9618d5e6c9b1246d33f97b0f30135ee756b2ac
ellietownsend <epaigetownsend@gmail.com>
2026-08-18 13:59:12 -0700

    add mode parameter to indicate if site is being ran in production or development

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,32 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      },
-      "/auth": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), "");
+  const VITE_API_URL = env.VITE_API_URL;
+
+
+  return {
+    plugins: [react()],
+
+    server: {
+      proxy: {
+        "/api": {
+          target:  VITE_API_URL,
+          changeOrigin: true,
+        },
+        "/auth": {
+          target:  VITE_API_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
+
+    build: {
+      outDir: "../server/dist",
+      emptyOutDir: true,
+      assetsDir: "assets",
+    },
+  };
 });
