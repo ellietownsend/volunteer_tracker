7cf138085b710639f69420c2483bd0331268536d
ellietownsend <epaigetownsend@gmail.com>
2026-08-18 17:15:54 -0700

    check sup abase key is being read by render

--- client/supabase-client.js
+++ client/supabase-client.js
@@ -4,6 +4,9 @@ import { createBrowserClient } from '@supabase/ssr'
 const supabaseURL = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("SUPABASE KEY EXISTS:", !!import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 const supabase = createBrowserClient(supabaseURL, supabaseKey);
 
 export default supabase;
